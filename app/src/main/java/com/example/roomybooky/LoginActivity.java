9d361a590682b83272c8e058f04766e792fc183a
nepucchi <100029794+nepucchi@users.noreply.github.com>
2023-04-01 20:13:02 +0700

    Added login activity

--- app/src/main/java/com/example/roomybooky/LoginActivity.java
+++ app/src/main/java/com/example/roomybooky/LoginActivity.java
@@ -1,14 +1,67 @@
 package com.example.roomybooky;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView user;
+    private TextView pass;
+    private Button submit;
+    private ProgressBar progressBar;
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        user = findViewById(R.id.IDuser);
+        pass = findViewById(R.id.IDpass);
+        submit = findViewById(R.id.IDsubmitbtn);
+        fAuth = FirebaseAuth.getInstance();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String username = user.getText().toString();
+                String password = user.getText().toString();
+                verification(username,password);
+            }
+        });
+    }
+
+    public void verification(String username, String password){
+        if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please fill in the required fields!", Toast.LENGTH_SHORT).show();
+        }else{
+            fAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressBar.setVisibility(View.GONE);
+                        startActivity(new Intent(LoginActivity.this, HomePage.class));
+                        finish();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Failed to Login :c", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
     }
 }
